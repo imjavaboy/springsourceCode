4aaed45404d28a0cb86cb4e7309ca10588364eec
1159311344@qq.com <1159311344@qq.com>
2022-08-23 09:09:28 +0800

    feat:切面之间的转换，代理创建的时机

--- src/main/java/org/springframework/aop/framework/autoproxy/A17.java
+++ src/main/java/org/springframework/aop/framework/autoproxy/A17.java
@@ -33,58 +33,36 @@ import java.util.List;
  */
 
 public class A17 {
-//    public static void main(String[] args) {
-//        GenericApplicationContext context = new GenericApplicationContext();
-//        context.registerBean("aspect1",Aspect1.class);
-//        context.registerBean("config",Config.class);
-//        context.registerBean(ConfigurationClassPostProcessor.class);
-//        //是依赖注入之前和初始化之后的后处理器
-//        context.registerBean(AnnotationAwareAspectJAutoProxyCreator.class);
-//        context.refresh();
-////        for (String name : context.getBeanDefinitionNames()) {
-////            System.out.println(name);
-////        }
-//
-//        //找到y有资格的切面 .findEligibleAdvisors
-//        AnnotationAwareAspectJAutoProxyCreator creator = context.getBean(AnnotationAwareAspectJAutoProxyCreator.class);
-//        List<Advisor> advisors = creator.findEligibleAdvisors(Target2.class, "target2");
-//        for (Advisor advisor : advisors) {
-//            System.out.println(advisor);
-//        }
-//
-//        /**前面方法返回不为空则表示需要创建代理*/
-//        Object o1 = creator.wrapIfNecessary(new Target1(), "target1", "target1");
-//        Object o2 = creator.wrapIfNecessary(new Target2(), "target2", "target2");
-//        System.out.println(o1.getClass());
-//        System.out.println(o2.getClass());
-//        ((Target1)o1).foo();
-//        ((Target2)o2).bar();
-//
-//    }
-
     public static void main(String[] args) {
-        AspectInstanceFactory factory = new SingletonAspectInstanceFactory(new Aspect1());
-        //模拟高级切面转成低级切面’
-        List<Advisor> list = new ArrayList<>();
-        for (Method method : Aspect.class.getDeclaredMethods()) {
+        GenericApplicationContext context = new GenericApplicationContext();
+        context.registerBean("aspect1",Aspect1.class);
+        context.registerBean("config",Config.class);
+        context.registerBean(ConfigurationClassPostProcessor.class);
+        //是依赖注入之前和初始化之后的后处理器
+        context.registerBean(AnnotationAwareAspectJAutoProxyCreator.class);
+        context.refresh();
+//        for (String name : context.getBeanDefinitionNames()) {
+//            System.out.println(name);
+//        }
 
-            if (method.isAnnotationPresent(Before.class)){
-                //解析切点
-                String expression = method.getAnnotation(Before.class).value();
-                //创建切点
-                AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
-                pointcut.setExpression(expression);
-                //通知
-                AspectJMethodBeforeAdvice advice = new AspectJMethodBeforeAdvice(method,pointcut,factory);
-                //切面
-                Advisor advisor = new DefaultPointcutAdvisor(pointcut,advice);
-                list.add(advisor);
-            }
-        }
-        for (Advisor advisor : list) {
+        //找到y有资格的切面 .findEligibleAdvisors
+        AnnotationAwareAspectJAutoProxyCreator creator = context.getBean(AnnotationAwareAspectJAutoProxyCreator.class);
+        List<Advisor> advisors = creator.findEligibleAdvisors(Target2.class, "target2");
+        for (Advisor advisor : advisors) {
             System.out.println(advisor);
         }
+
+        /**前面方法返回不为空则表示需要创建代理*/
+        Object o1 = creator.wrapIfNecessary(new Target1(), "target1", "target1");
+        Object o2 = creator.wrapIfNecessary(new Target2(), "target2", "target2");
+        System.out.println(o1.getClass());
+        System.out.println(o2.getClass());
+        ((Target1)o1).foo();
+        ((Target2)o2).bar();
+
     }
+
+
     static class Target1{
         public void foo(){
             System.out.println("target1 foo");
---
+++ src/main/java/org/springframework/aop/framework/autoproxy/A17_2.java
@@ -0,0 +1,75 @@
+package org.springframework.aop.framework.autoproxy;
+
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectInstanceFactory;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.aspectj.AspectJMethodBeforeAdvice;
+import org.springframework.aop.aspectj.SingletonAspectInstanceFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 郭本琪
+ * @description
+ * @date 2022/8/22
+ * @Copyright 总有一天，会见到成功
+ */
+
+public class A17_2 {
+
+    @org.aspectj.lang.annotation.Aspect
+    static class Aspect{
+        @Before("execution(* foo())")
+        public void before1(){
+            System.out.println("aspect before1...");
+        }
+        @Before("execution(* foo())")
+        public void  before2(){
+            System.out.println("aspect before2...");
+        }
+        public void  after(){
+            System.out.println("aspect after...");
+        }
+        public void  afterRunning(){
+            System.out.println("aspect afterRunning...");
+        }
+        public void  afterThrowing(){
+            System.out.println("aspect afterThrowing...");
+        }
+    }
+    static class Target{
+        public void foo(){
+            System.out.println("target foo");
+        }
+    }
+    public static void main(String[] args) {
+        AspectInstanceFactory factory = new SingletonAspectInstanceFactory(new Aspect());
+        //模拟高级切面转成低级切面’
+        List<Advisor> list = new ArrayList<>();
+        for (Method method : Aspect.class.getDeclaredMethods()) {
+
+            if (method.isAnnotationPresent(Before.class)){
+                //解析切点
+                String expression = method.getAnnotation(Before.class).value();
+                //创建切点
+                AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+                pointcut.setExpression(expression);
+                //通知
+                AspectJMethodBeforeAdvice advice = new AspectJMethodBeforeAdvice(method,pointcut,factory);
+                //切面
+                Advisor advisor = new DefaultPointcutAdvisor(pointcut,advice);
+                list.add(advisor);
+            }
+        }
+        for (Advisor advisor : list) {
+            System.out.println(advisor);
+        }
+    }
+}
